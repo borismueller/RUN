312c27eee5764ef4f900351bec25d0dedd02bc61
julienkellerhals <julien.kellerhals@gmail.com>
2017-04-19 16:26:31 +0200

    restructure userIndex and remove tag adaptation

--- view/user_index.php
+++ view/user_index.php
@@ -1,27 +1,22 @@
-<?php
-if (!empty($files)){
-	foreach ($files as $file){
-		  $fileName = $file->name;
-			$fileTag = $file->tags;
-			$filePath = $file->path;
-
-			echo '   <div class="object">
-			      <div class="item">
-			        <div class="item-type">itemtype</div>
-			        <div class="item-desc">
-			          <div class="item-desc-name">'.$fileName.'</div>
-			          <div class="item-desc-icon">icon</div>
-			        </div>
-			      </div>
-			      <div class="tag">
-			        <div class="tag-text">'.$fileTag.'</div>
-			        <div class="tag-icon">icon</div>
-			      </div>
-			    </div>';
-	}
-}
-?>
-
+<?php if (!empty($files)) : ?>
+	<?php foreach ($files as $file) : ?>
+		<div class="object">
+			<div class="item">
+				<div class="item-type">itemtype</div>
+				<div class="item-desc">
+					<div class="item-desc-name"><?= $file->name ?></div>
+					<div class="item-desc-icon">icon</div>
+				</div>
+			</div>
+			<?php if (!empty($file->tags)) : ?>
+				<div class="tag">
+					<div class="tag-text"><?= $file->tags ?></div>
+					<div class="tag-icon">&#10005;</div>
+				</div>
+			<?php endif ?>
+		</div>
+	<?php endforeach ?>
+<?php endif ?>
 <div class="object">
-  <div class="plus" onclick="location='/user/upload'">+</div>
+	<div class="plus" onclick="location='/user/upload'">+</div>
 </div>
