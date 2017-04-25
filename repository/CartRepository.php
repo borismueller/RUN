b59aa42a8a2e6866acabbf1339fbb2fcd134b655
borismueller <19bo99@gmail.com>
2017-04-25 15:20:42 +0200

    machine learning stuff

---
+++ repository/CartRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once '../lib/Repository.php';
+
+/**
+ * Das UserRepository ist zuständig für alle Zugriffe auf die Tabelle "user".
+ *
+ * Die Ausführliche Dokumentation zu Repositories findest du in der Repository Klasse.
+ */
+class CartRepository extends Repository
+{
+    /**
+     * Diese Variable wird von der Klasse Repository verwendet, um generische
+     * Funktionen zur Verfügung zu stellen.
+     */
+    protected $tableName = 'cart';
+
+		public function readByUser_Id($user_id)
+    {
+        // Query erstellen
+        $query = "SELECT * FROM {$this->tableName} WHERE user_id=?";
+
+        // Datenbankverbindung anfordern und, das Query "preparen" (vorbereiten)
+        // und die Parameter "binden"
+        $statement = ConnectionHandler::getConnection()->prepare($query);
+        $statement->bind_param('i', $user_id);
+
+        // Das Statement absetzen
+        $statement->execute();
+
+        // Resultat der Abfrage holen
+        $result = $statement->get_result();
+        if (!$result) {
+            throw new Exception($statement->error);
+        }
+
+        // Ersten Datensatz aus dem Reultat holen
+				$rows = array();
+        while ($row = $result->fetch_object()) {
+            $rows[] = $row;
+        }
+
+        return $rows;
+    }
+}
