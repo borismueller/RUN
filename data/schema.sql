6a248cc3df07f2b0cc53d561a8e38dff06f4d6b3
borismueller <19bo99@gmail.com>
2017-04-25 15:41:44 +0200

    Revert "Revert "Revert "machine learning stuff"""
    This reverts commit ded8861706862fc11a7509c0769d0a29477b1830.

--- data/schema.sql
+++ data/schema.sql
@@ -34,16 +34,8 @@ CREATE TABLE cart (
 
 #--cart will be used for storage after logout
 #--makes a user with the password test
-INSERT INTO user (name, password) VALUES ('test', '$2y$10$/wt7Nannt9mfYCEw0e/B6.PBpPCMnRuyAmHKBEI8O6Vm0R6BQkAdi'),
-	('test2', '$2y$10$/wt7Nannt9mfYCEw0e/B6.PBpPCMnRuyAmHKBEI8O6Vm0R6BQkAdi'), ('test3', '$2y$10$/wt7Nannt9mfYCEw0e/B6.PBpPCMnRuyAmHKBEI8O6Vm0R6BQkAdi'),
-	('test4', '$2y$10$/wt7Nannt9mfYCEw0e/B6.PBpPCMnRuyAmHKBEI8O6Vm0R6BQkAdi');
+INSERT INTO user (name, password) VALUES ('test', '$2y$10$/wt7Nannt9mfYCEw0e/B6.PBpPCMnRuyAmHKBEI8O6Vm0R6BQkAdi');
 
 INSERT INTO type (name) VALUES ('type1'), ('type2'), ('sht'), ('stuff');
 
-INSERT INTO product (name, type_id, price) VALUES ('prod1', 1, 42.69), ('prod2', 3, 420), ('The kid who drowned at summercamp', 4, 3.1431841), ('prod4', 3, 100);
-
-INSERT INTO cart (user_id, product_id) VALUES (1, 1), (1, 2); #--user 1
-INSERT INTO cart (user_id, product_id) VALUES (2, 1), (2, 2); #--user 2
-INSERT INTO cart (user_id, product_id) VALUES (3, 3), (3, 4); #--user 3
-INSERT INTO cart (user_id, product_id) VALUES (4, 4), (4, 3); #--user 4
-#-- 1 & 2, 3 & 4, are bought together
+INSERT INTO product (name, type_id, price) VALUES ('naem', 1, 42.69), ('amazing prod', 3, 420), ('The kid who drowned at summercamp', 4, 3.1431841);
