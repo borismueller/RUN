dce52f233e167a6d8d7591f3ff57f37044f8094a
casimirblaser <casimir.blaser@iet-gibb.ch>
2017-04-26 09:19:16 +0200

    Slider Full Screen

--- view/default_index.php
+++ view/default_index.php
@@ -6,8 +6,7 @@
       margin: auto;
   }
   </style>
-  <div class="container">
-    <br>
+  <div class="slider-container">
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
       <!-- Indicators -->
       <ol class="carousel-indicators">
@@ -56,11 +55,11 @@
 
       <!-- Left and right controls -->
       <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
       </a>
       <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
       </a>
     </div>
